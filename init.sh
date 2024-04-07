07c39733ce377a1d35dea99596ed52b974b19b83
George Cosma <george@whitecitycode.com>
2024-04-07 10:54:45 +0300

    initial packages

--- init.sh
+++ init.sh
@@ -13,5 +13,20 @@ if [ "$(uname)" == "Linux" ]; then
     sudo dnf install -y git zoxide bat eza fzf ripgrep neovim tmux highlight zsh-syntax-highlighting zsh-autosuggestions 
 
 elif [ "$(uname)" == "Darwin" ]; then
-    echo "Mac"
+    if [ "$(which brew)" ]; then
+        echo "Brew already installed, skipping..."
+    else
+        echo "Installing Brew..."; \
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+
+    if [ "$(which zsh)" ]; then
+        echo "Zsh already installed, skipping..."
+    else
+        echo "Installing Zsh..."; \
+        brew install zsh; \
+        sudo chsh -s $(which zsh)
+    fi
+
+    brew install git zoxide bat eza fzf ripgrep neovim tmux highlight zsh-syntax-highlighting zsh-autosuggestions
 fi
