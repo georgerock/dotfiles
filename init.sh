ed2bb16556bef47db358d37d4ec0cb56ed69b91d
George Cosma <george@whitecitycode.com>
2024-04-07 12:29:37 +0300

    added mac version

--- init.sh
+++ init.sh
@@ -66,6 +66,15 @@ elif [ "$(uname)" == "Darwin" ]; then
     # Starship
     curl -sS https://starship.rs/install.sh | sh
 
+    # Node version manager
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+    # Deno
+    curl -fsSL https://deno.land/install.sh | sh
+
+    # Rust
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
     # tmux
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
