c3beb66e681eacb877f95f9ff0bc1f8a4648660d
Cosma George <george@whitecitycode.com>
2024-05-31 14:27:56 +0300

    feat(wezterm): added wezterm

--- .config/nvim/lua/georgerock/set.lua
+++ .config/nvim/lua/georgerock/set.lua
@@ -37,7 +37,7 @@ vim.opt.undofile = true
 
 -- Inlay hints
 vim.api.nvim_create_autocmd('LspAttach', {
-    vim.api.nvim_set_hl(0, 'LspInlayHint', { fg = '#585b70' }),
+    vim.api.nvim_set_hl(0, 'LspInlayHint', { bg = '#313244', fg = '#9399b2' }),
 
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
     callback = function(args)
---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("MonoLisa Nerd Font Mono", { weight = "Bold", stretch = "SemiExpanded" })
+config.font_size = 16
+
+config.color_scheme = "catppuccin-mocha"
+config.automatically_reload_config = true
+config.line_height = 2
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+
+return config
