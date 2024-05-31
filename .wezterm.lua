f57441d98cc30dbcc5c16a9d9f6a0fc9bae0e36b
Cosma George <george@whitecitycode.com>
2024-05-31 18:03:43 +0300

    chore(nvim): removed some fat

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("MonoLisa Nerd Font Mono", { weight = "Bold", stretch = "SemiExpanded" })
-config.font_size = 16
+config.font_size = 19
 
 config.color_scheme = "catppuccin-mocha"
 config.automatically_reload_config = true
