24e8ffc972ee916df413c7446f34c9fc2e491099
Cosma George <cosma.george93@gmail.com>
2024-12-09 08:37:05 +0200

    changed tmux topbar

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,6 +4,13 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("MonoLisa Nerd Font Mono", { weight = "Bold", stretch = "SemiExpanded" })
 config.font_size = 20
 
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 config.color_scheme = "catppuccin-mocha"
 config.automatically_reload_config = true
 config.line_height = 2
